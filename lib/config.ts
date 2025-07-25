61305f986153cd695ec540cd3e1d7ad658e433a1
blackcatfilm <165353378+blackcatfilm@users.noreply.github.com>
2025-07-25 18:06:29 +0800

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,7 @@
 // TODO: change these to your own values
 // NOTE: rootNotionSpaceId is optional; set it to undefined if you don't want to
 // use it.
-export const rootNotionPageId = 'f21d69aaa7214868b465490a9bc6eee2'
+export const rootNotionPageId = '23afe9c3783d80818213e17c70ecc455'
 export const rootNotionSpaceId = ''
 
 // NOTE: having this enabled can be pretty expensive as it re-generates preview
